68732278adca43cd6d2d1588482df77a3bf94b91
Roberto <robq97@hotmail.com>
2019-10-21 18:31:02 -0600

    Se creó activity de Login

--- app/src/main/java/com/project/incidenciascr/MainActivity.java
+++ app/src/main/java/com/project/incidenciascr/MainActivity.java
@@ -1,14 +1,30 @@
 package com.project.incidenciascr;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.btn_iniciar_sesion);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLogin();
+            }
+        });
+    }
+
+    public void openLogin(){
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
     }
 }
