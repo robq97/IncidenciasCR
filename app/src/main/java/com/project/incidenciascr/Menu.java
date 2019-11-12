69cb4c709ace110e0e491cede614e3c1975543b7
Roberto <robq97@hotmail.com>
2019-11-12 14:17:35 -0600

    Se creó activity para la activación de la cuenta

--- app/src/main/java/com/project/incidenciascr/Menu.java
+++ app/src/main/java/com/project/incidenciascr/Menu.java
@@ -2,13 +2,31 @@ package com.project.incidenciascr;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Menu extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        button = (Button) findViewById(R.id.btn_nueva_incidencia);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivation();
+            }
+        });
+    }
+
+    public void openActivation(){
+        Intent intent = new Intent(this, AccountActivation.class);
+        startActivity(intent);
     }
 }
