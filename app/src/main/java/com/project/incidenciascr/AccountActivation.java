5724875ca87e8bcfcb5cc3fa1493019aa6c03d53
Roberto <robq97@hotmail.com>
2019-11-12 15:58:11 -0600

    Se agregó maeView a la nueva incidencia

--- app/src/main/java/com/project/incidenciascr/AccountActivation.java
+++ app/src/main/java/com/project/incidenciascr/AccountActivation.java
@@ -2,13 +2,31 @@ package com.project.incidenciascr;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AccountActivation extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_activation);
+
+        button = (Button) findViewById(R.id.btn_confirmar);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNewIncidence();
+            }
+        });
+    }
+
+    public void openNewIncidence(){
+        Intent intent = new Intent(this, NewIncidence.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/project/incidenciascr/NewIncidence.java
+++ app/src/main/java/com/project/incidenciascr/NewIncidence.java
@@ -3,12 +3,110 @@ package com.project.incidenciascr;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.widget.Toast;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import java.io.IOException;
+import android.widget.ImageView;
+import android.widget.Gallery;
+
+import com.google.android.gms.maps.GoogleMap;
+
+import static android.media.MediaRecorder.VideoSource.CAMERA;
+
+
 
 public class NewIncidence extends AppCompatActivity {
+
+    private Button button;
+    GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_incidence);
+
+        button = (Button) findViewById(R.id.btn_agregar_img);
+        GoogleMap map;
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog();
+            }
+
+        });
+    }
+
+    private void showDialog(){
+        AlertDialog.Builder pictureDialog = new AlertDialog.Builder(this);
+        pictureDialog.setTitle("Eliga una opción");
+        String[] pictureDialogItems = {
+                "Foto de galería",
+                "Foto de cámara" };
+        pictureDialog.setItems(pictureDialogItems,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch (which) {
+                            case 0:
+                                fotoGaleria();
+                                break;
+                            case 1:
+                                fotoCamara();
+                                break;
+                        }
+                    }
+                });
+        pictureDialog.show();
+    }
+
+    public void fotoGaleria() {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+
+        //startActivityForResult(galleryIntent, GALLERY);
+    }
+
+    private void fotoCamara() {
+        Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, CAMERA);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        /*super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == this.RESULT_CANCELED) {
+            return;
+        }
+        if (requestCode == GALLERY) {
+            if (data != null) {
+                Uri contentURI = data.getData();
+                try {
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), contentURI);
+                    String path = saveImage(bitmap);
+                    Toast.makeText(this, "Image Saved!", Toast.LENGTH_SHORT).show();
+                    imageview.setImageBitmap(bitmap);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Toast.makeText(this, "Failed!", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        } else if (requestCode == CAMERA) {
+            Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+            imageview.setImageBitmap(thumbnail);
+            saveImage(thumbnail);
+            Toast.makeText(this, "Image Saved!", Toast.LENGTH_SHORT).show();
+        }*/
     }
 }
