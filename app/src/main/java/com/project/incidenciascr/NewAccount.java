ff086dbf805ea4e4510f1cce35057b6756ac09e2
amonteroa <amonteroa548@ulacit.ed.cr>
2019-11-23 10:43:26 -0800

    se agregaron metodos de consultar, y modificar. se elimino un metodo de agregar

--- app/src/main/java/com/project/incidenciascr/NewAccount.java
+++ app/src/main/java/com/project/incidenciascr/NewAccount.java
@@ -1,11 +1,5 @@
 package com.project.incidenciascr;
 
-import android.widget.EditText;
-import android.database.sqlite.SQLiteDatabase;
-import android.widget.Spinner;
-import android.text.TextUtils;
-import android.widget.Toast;
-import android.content.ContentValues;
 import androidx.appcompat.app.AppCompatActivity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
@@ -24,10 +18,6 @@ public class NewAccount extends AppCompatActivity {
     private Button button;
     private EditText nombre,primerApellido, segundoApellido, cedula, telefono,
     correo, direccion, password, confirmaPassword;
-
-    private EditText input_nombre, input_primer_apellido, input_segundo_apellido, input_cedula, input_tel_cel, input_email,
-            input_password_nueva_cuenta, input_password_nueva_cuenta_confirmacion, txt_direccion;
-    private Spinner spinner_provincias, spinner_cantones, spinner_distritos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -96,20 +86,6 @@ public class NewAccount extends AppCompatActivity {
 
             }
         });
-
-        input_nombre = (EditText) findViewById(R.id.input_nombre);
-        input_primer_apellido = (EditText) findViewById(R.id.input_primer_apellido);
-        input_segundo_apellido = (EditText) findViewById(R.id.input_segundo_apellido);
-        input_cedula = (EditText) findViewById(R.id.input_cedula);
-        input_tel_cel = (EditText) findViewById(R.id.input_tel_cel);
-        input_email = (EditText) findViewById(R.id.input_email);
-        input_password_nueva_cuenta = (EditText) findViewById(R.id.input_password_nueva_cuenta);
-        input_password_nueva_cuenta_confirmacion = (EditText) findViewById(R.id.input_password_nueva_cuenta_confirmacion);
-        txt_direccion = (EditText) findViewById(R.id.txt_direccion);
-
-        spinner_provincias = (Spinner) findViewById(R.id.spinner_provincias);
-        spinner_cantones = (Spinner) findViewById(R.id.spinner_cantones);
-        spinner_distritos = (Spinner) findViewById(R.id.spinner_distritos);
     }
 
 
@@ -149,78 +125,5 @@ public class NewAccount extends AppCompatActivity {
     public void openMenu(){
         Intent intent = new Intent(this, Menu.class);
         startActivity(intent);
-    }
-
-    private boolean passwordMatch() {
-        if(input_password_nueva_cuenta.getText().toString().equals(input_password_nueva_cuenta_confirmacion.getText().toString())) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    private void nuevaCuenta(View v) {
-        BDConexion cnn = new BDConexion(this, "Admin", null, 1);
-        SQLiteDatabase bd = cnn.getWritableDatabase();
-        ContentValues valores = new ContentValues();
-
-        try {
-            //validar campos en blanco
-            if(!TextUtils.isEmpty(input_nombre.getText().toString()) ||
-                    !TextUtils.isEmpty(input_primer_apellido.getText().toString()) ||
-                    !TextUtils.isEmpty(input_segundo_apellido.getText().toString()) ||
-                    !TextUtils.isEmpty(input_cedula.getText().toString()) ||
-                    !TextUtils.isEmpty(input_tel_cel.getText().toString()) ||
-                    !TextUtils.isEmpty(input_email.getText().toString())
-            ) {
-                //validar si contraseñas en blanco, y luego si son iguales
-                if(!TextUtils.isEmpty(input_password_nueva_cuenta.getText().toString()) || !TextUtils.isEmpty(input_password_nueva_cuenta_confirmacion.getText().toString())) {
-                    if(passwordMatch()) {
-
-                        valores.put("nombre", input_nombre.getText().toString());
-                        valores.put("primer_apellido", input_primer_apellido.getText().toString());
-                        valores.put("segundo_apellido", input_segundo_apellido.getText().toString());
-                        valores.put("cedula", input_cedula.getText().toString());
-                        valores.put("tel_cel", input_tel_cel.getText().toString());
-                        valores.put("correo_electronico", input_email.getText().toString());
-                        valores.put("clave", input_password_nueva_cuenta.getText().toString());
-
-                        valores.put("provincia", spinner_provincias.getSelectedItem().toString());
-                        valores.put("canton", spinner_cantones.getSelectedItem().toString());
-                        valores.put("distrito", spinner_distritos.getSelectedItem().toString());
-
-                        if(!TextUtils.isEmpty(txt_direccion.getText().toString())) {
-                            valores.put("direccion", txt_direccion.getText().toString());
-                        }
-
-                        bd.insert("Cuenta", null, valores);
-
-                        bd.close();
-
-                        Toast.makeText(this, "Se agregó exitosamente.", Toast.LENGTH_LONG).show();
-                    } else {
-                        Toast.makeText(this, "Contraseñas no coinciden.", Toast.LENGTH_LONG).show();
-                    }
-                } else {
-                    Toast.makeText(this, "A excepción de su dirección, debe llenar todos los campos solicitados.", Toast.LENGTH_LONG).show();
-                }
-
-            }
-
-            input_nombre.setText("");
-            input_primer_apellido.setText("");
-            input_segundo_apellido.setText("");
-            input_cedula.setText("");
-            input_tel_cel.setText("");
-            input_email.setText("");
-            input_password_nueva_cuenta.setText("");
-            input_password_nueva_cuenta_confirmacion.setText("");
-            txt_direccion.setText("");
-
-        }
-
-        catch (Exception ex) {
-            Toast.makeText(this, "Please end my life.", Toast.LENGTH_LONG).show();
-        }
     }
 }
