589b6c035cc4da84f4a3167114245d1e86e4b97f
amonteroa <amonteroa548@ulacit.ed.cr>
2019-11-23 10:37:42 -0800

    Se agregzron validaciones a login, NewAccount, NewIncidente, AccountActivation,

---
+++ app/src/main/java/com/project/incidenciascr/Contacto.java
@@ -0,0 +1,88 @@
+package com.project.incidenciascr;
+
+public class Contacto {
+
+    private int cedula;
+    private String nombre, primerApellido, segundoApellido, correo, direccion, password;
+    private int telefono;
+
+    public Contacto() {
+    }
+
+    public Contacto(int cedula, String nombre, String primerApellido, String segundoApellido, int telefono,
+                    String correo, String direccion, String password ) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.primerApellido = primerApellido;
+        this.segundoApellido = segundoApellido;
+        this.correo = correo;
+        this.direccion = direccion;
+        this.password = password;
+        this.telefono = telefono;
+    }
+
+    public int getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPrimerApellido(){
+        return primerApellido;
+    }
+
+    public void setPrimerApellido(String primerApellido) {
+        this.primerApellido = primerApellido;
+    }
+
+    public String getSegundoApellido(){
+        return segundoApellido;
+    }
+
+    public void setSegundoApellido(String segundoApellido) {
+        this.segundoApellido = segundoApellido;
+    }
+
+    public String getCorreo(){
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getDireccion(){
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+}
