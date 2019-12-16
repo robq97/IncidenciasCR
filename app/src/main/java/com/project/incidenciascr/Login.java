ae87c729ee9a3c29615dd0e375b7df5a25709ff2
Roberto <robq97@hotmail.com>
2019-12-16 16:33:58 -0600

    Se arregló metodo para login en acivity Login

--- app/src/main/java/com/project/incidenciascr/Login.java
+++ app/src/main/java/com/project/incidenciascr/Login.java
@@ -16,8 +16,8 @@ import android.database.Cursor;
 
 public class Login extends AppCompatActivity {
 
-    private Button btnNuevaCuenta, btnIngresar, btn_ingresar;
-    private EditText email, password , input_email, input_password;
+    private Button btnNuevaCuenta, btn_ingresar;
+    private EditText input_email, input_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +25,6 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         btnNuevaCuenta = (Button)findViewById(R.id.btn_nueva_cuenta);
-        email = (EditText)findViewById(R.id.input_email);
-        password = (EditText)findViewById(R.id.input_password);
-        btnIngresar = (Button)findViewById(R.id.btn_ingresar);
         input_email = (EditText) findViewById(R.id.input_email);
         input_password = (EditText) findViewById(R.id.input_password);
         btn_ingresar = (Button) findViewById(R.id.btn_ingresar);
@@ -45,55 +42,24 @@ public class Login extends AppCompatActivity {
                 openNewAccount();
             }
         });
+    }
 
-        btnIngresar.setOnClickListener(new View.OnClickListener(){
-          @Override
-          public void onClick(View v) {
-
-              //string que valida correo con expresiones regulares
-              //espera un set de caracteres en las primeras llaves, luego un @
-              //luego otro set de caracteres, luego un punto, y luego otro set de caracteres
-              String verificaEmail = "[a-zA-Z0-9\\+\\@\\-\\+]{1,256}"+
-                      "\\@" +
-                      "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" + "(" + "\\." + "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
-                      ")+";
-
-              //se agarra lo que el usuario haya introducido en el campo y se convierte a un string
-              String mail = email.getText().toString();
-              Matcher verifica = Pattern.compile(verificaEmail).matcher(mail);
-              if(verifica.matches()){
-                  if(verificaPassword()){
-                      openMenu();
-                  }
-              }else{
-                  Toast.makeText(getApplicationContext(), "Por favor ingrese un email valido", Toast.LENGTH_LONG).show();
-                  email.setText(null);
-              }
-          }
-        });
+    private boolean emailCheck(){
+        String verificaEmail = "[a-zA-Z0-9\\+\\@\\-\\+]{1,256}"+
+                "\\@" +
+                "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" + "(" + "\\." + "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
+                ")+";
+        Matcher verifica = Pattern.compile(verificaEmail).matcher(input_email.getText().toString());
+        if (!verifica.matches()){
+            Toast.makeText(getApplicationContext(), "Por favor ingrese un correo electronico valido", Toast.LENGTH_LONG).show();
+            return false;
+        } else {
+            return true;
+        }
     }
 
     public void openMenu(){
         Intent intent = new Intent(this, Menu.class);
-        startActivity(intent);
-    }
-
-    private boolean verificaPassword(){
-        String contra = password.getText().toString().trim();
-        if(contra.isEmpty()){
-            Toast.makeText(getApplicationContext(), "Por favor ingrese una contraseña", Toast.LENGTH_LONG).show();
-            return false;
-        }else{
-            Toast.makeText(getApplicationContext(), "contraseña valida", Toast.LENGTH_LONG).show();
-
-            return true;
-        }
-
-
-    }
-
-    public void openNewAccount(){
-        Intent intent = new Intent(this, NewAccount.class);
         startActivity(intent);
     }
 
@@ -105,21 +71,36 @@ public class Login extends AppCompatActivity {
         try {
             //validar si campos están llenos
             if(!TextUtils.isEmpty(input_email.getText().toString()) || !TextUtils.isEmpty(input_password.getText().toString()) ) {
-
-                Cursor fila = bd.rawQuery("SELECT * FROM Cuenta WHERE correo_electronico =?", new String[] {input_email.getText().toString()});
-
-                if(input_password.getText().toString() == fila.getString(10)) {
-                    return true;
-                } else {
-                    return false;
+                //valido email valido
+                if (emailCheck()){
+                    String correo = input_email.getText().toString();
+                    String password = input_password.getText().toString();
+                    Cursor fila = bd.rawQuery("SELECT * FROM Cuenta WHERE correo_electronico = ? AND clave = ? ", new String[] {correo, password});
+                    if (fila.getCount() > 0){
+                        fila.moveToFirst();
+                        Toast.makeText(this, "Bienvenido.", Toast.LENGTH_LONG).show();
+                        openMenu();
+                        input_email.setText("");
+                        input_password.setText("");
+                        return true;
+                    } else {
+                        Toast.makeText(this, "Contraseña y/o correo invalido.", Toast.LENGTH_LONG).show();
+                        return false;
+                    }
                 }
-            } else {
+            }else {
                 Toast.makeText(this, "Debe llenar todos los campos solicitados.", Toast.LENGTH_LONG).show();
                 return false;
             }
+
         } catch (Exception ex) {
             return false;
         }
+        return false;
+    }
 
+    public void openNewAccount(){
+        Intent intent = new Intent(this, NewAccount.class);
+        startActivity(intent);
     }
 }
